feda1f2a6d6b0d78eabd72729fb658fe69f86d58
Gabbag2 <gabriel.rochon97410@gmail.com>
2025-10-15 17:55:22 +0200

    week6

---
+++ Week-6/Exercises/stroop.py
@@ -0,0 +1,36 @@
+from expyriment import design, control, stimuli
+from expyriment.misc.constants import C_WHITE, C_BLACK, C_GREEN, C_RED , C_BLUE
+from expyriment.misc.constants import K_LEFT, K_RIGHT
+from expyriment.misc import Colour
+import random
+
+exp = design.Experiment(name="Blindspot", background_colour=C_WHITE, foreground_colour=C_BLACK)
+control.set_develop_mode()
+control.initialize(exp)
+
+def stroop():
+    C_ORANGE = Colour((255, 165, 0))
+    for j in range(2):
+        for i in range(10):
+            stimuli.TextScreen("Instructions", "Press K_right if it's good and K_left if it's bad").present()
+        
+            fixation = stimuli.FixCross(size=(50, 50), line_width=10, position=[0, 0])
+            text = ["red", "blue", "green", "orange"]
+            colors = [C_RED, C_BLUE, C_GREEN, C_ORANGE]
+            word = random.choice(text)
+            color = random.choice(colors)
+            stim = stimuli.TextLine(word, text_colour=color, text_size=100)
+            fixation.present(True, True)
+            exp.clock.wait(500)
+            stim.present(True, True)
+            key, rt = exp.keyboard.wait(keys = [K_LEFT, K_RIGHT])
+            type_trial = "True" if (word == "red" and color == C_RED) or (word == "blue" and color == C_BLUE) or (word == "green" and color == C_GREEN) or (word == "orange" and color == C_ORANGE) else "False"
+            accuracy = "True" if (type_trial == "True" and key == K_RIGHT) or (type_trial == "False" and key == K_LEFT) else "False"
+            exp.add_data_variable_names(["trial block", "trial number", "trial type", "word meaning", "text color", "RTs", "accuracy"]) 
+            exp.data.add([j,i,type_trial,word,color,rt,accuracy]) 
+            affichage = stimuli.TextLine("Your answer is " + accuracy + " and your reaction time is " + str(rt) + " ms", text_colour=C_BLACK, text_size=30)
+            affichage.present(True, True)
+            exp.clock.wait(2000)
+control.start(subject_id=1)       
+stroop()
+control.end()
